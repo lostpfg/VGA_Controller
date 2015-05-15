f28d708bcc140031ec8ec794e29c6491b4dd39e5
unknown <lostpfg90@gmail.com>
2015-05-15 23:56:27 +0300

    top module updated/fixed, also ucf file is corrected to nets of board

--- topModule.v
+++ topModule.v
@@ -8,7 +8,7 @@ input 					ps2Data;
 wire 						pixelClk;
 wire		[7:0] 	ps2Code;
 wire		[5:0] 	ps2OutCode;
-wire		[2:0] 	outColr;
+wire		[2:0] 	outColor;
 
 output	[8:0]  	vgaRGB;
 output 		    	vgaHsync;
@@ -16,7 +16,7 @@ output 		    	vgaVsync;
 
 pixelClk				clk25MHz 	( boardClk, reset, pixelClk );
 kbdController		kdbOv			(	boardClk,	reset, ps2Clk, ps2Data, ps2OutCode );
-ps2Decode 			coDec			( reset, ps2OutCode, outColr );
+colorDecode 		clrDec 		( reset, 		ps2OutCode, outColor );
 dispController	vgaOv    	(	pixelClk,	reset, outColr, vgaRGB, vgaHsync, vgaVsync );
 
 endmodule
